283fc4d988595292fd617ef87370e8df90c794fb
Filip Tarajko <filiptarajko@gmail.com>
2021-03-29 20:36:33 +0200

    add basic functionality and reset button

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const content = document.querySelector('#content');
+const button = document.querySelector('#reset');
 
-for(i=0;i<16;i++)
-{
-    for(j=0;j<16;j++){
-        let square = document.createElement('div');
-        square.classList.add('square')
-        content.appendChild(square);
+function paint(e){
+    e.target.classList.add('painted');
+}
+
+function resetBoard(){
+    squares.forEach((square)=>
+    {
+        square.classList.remove('painted');
+    })
+}
+
+function newBoard(n){
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++){
+            let square = document.createElement('div');
+            square.classList.add('square')
+            content.appendChild(square);
+        }
     }
 }
+
+newBoard(16);
+squares = document.querySelectorAll('.square')
+squares.forEach((square)=>{
+    square.addEventListener('mouseover', paint);
+});
+
+button.addEventListener('click', resetBoard);
