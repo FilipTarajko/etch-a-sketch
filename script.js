1d8e3331b35467f23469ab6d4406d97c5d2703c3
Filip Tarajko <filiptarajko@gmail.com>
2021-03-29 20:51:25 +0200

    add button for changing size

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const content = document.querySelector('#content');
 const button = document.querySelector('#reset');
+const newsize = document.querySelector('#newsize');
 
 function paint(e){
     e.target.classList.add('painted');
@@ -12,21 +13,42 @@ function resetBoard(){
     })
 }
 
+function changeSize(){
+    removeSquares();
+    newBoard(window.prompt("Choose new size (min: 1, max: 100)"));
+}
+
 function newBoard(n){
+    if(n<1){
+        n=1;
+    }
+    if(n>100){
+        n=100;
+    }
+    content.style.gridTemplateColumns= `repeat(${n}, ${640/n}px)`;
     for(i=0;i<n;i++)
     {
         for(j=0;j<n;j++){
             let square = document.createElement('div');
             square.classList.add('square')
+            square.style.height = `${640/n-2}px`;
+            square.style.width = `${640/n-2}px`;
             content.appendChild(square);
         }
     }
+    squares = document.querySelectorAll('.square')
+    squares.forEach((square)=>{
+        square.addEventListener('mouseover', paint);
+    });
+}
+
+function removeSquares(){
+    squares.forEach((square)=>{
+        content.removeChild(square);
+    })
 }
 
 newBoard(16);
-squares = document.querySelectorAll('.square')
-squares.forEach((square)=>{
-    square.addEventListener('mouseover', paint);
-});
 
 button.addEventListener('click', resetBoard);
+newsize.addEventListener('click', changeSize);
